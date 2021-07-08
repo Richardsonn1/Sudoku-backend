1ef5e6bc1977d33793bccf71299add1b306ad253
Richardsonn1 <marcus.richardson@hotmail.se>
2021-07-08 16:23:15 +0200

    GameDto Added.

---
+++ src/main/java/se/junior/developer/sudokubackend/dtos/GameDto.java
@@ -0,0 +1,12 @@
+package se.junior.developer.sudokubackend.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GameDto {
+
+    private Long map;
+    private double finishTime;
+}
