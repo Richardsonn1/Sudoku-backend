cb3894ea3cd590ffa7734010ff89cb565cb34bb8
Richardsonn1 <marcus.richardson@hotmail.se>
2021-07-09 15:09:29 +0200

    GameService updated.

--- src/main/java/se/junior/developer/sudokubackend/services/GameService.java
+++ src/main/java/se/junior/developer/sudokubackend/services/GameService.java
@@ -3,6 +3,7 @@ package se.junior.developer.sudokubackend.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import se.junior.developer.sudokubackend.dtos.GameDto;
+import se.junior.developer.sudokubackend.mappers.GameMapper;
 import se.junior.developer.sudokubackend.models.Game;
 import se.junior.developer.sudokubackend.repositories.GameRepo;
 
@@ -17,6 +18,6 @@ public class GameService {
     }
 
     public Game addGame(GameDto gameDto) {
-        return null;
+        return gameRepo.save(GameMapper.INSTANCE.dtoToGame(gameDto));
     }
 }
